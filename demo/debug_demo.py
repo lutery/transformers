d124d01afa55f1f807c5fc7eb3aa93b5db81a008
luctery <luctery@foxmail.com>
2025-04-20 22:34:36 +0800

    正在添加注释

--- demo/debug_demo.py
+++ demo/debug_demo.py
@@ -14,10 +14,10 @@ def run_llama():
     llamaConfig = LlamaConfig(
         vocab_size=32000, # 词表大小
         hidden_size=4096 // 2, # 隐藏层大小
-        intermediate_size=11008 // 2,
-        num_hidden_layers=32 // 2,
-        num_attention_heads=32 // 2,
-        max_position_embeddings=2048 // 2,
+        intermediate_size=11008 // 2, # 前馈网络（Feed-Forward Network, FFN）中间层的维度大小。作用：定义每个 Transformer 层中前馈网络的隐藏层大小，通常比 hidden_size 大
+        num_hidden_layers=32 // 2, # Transformer 模型的隐藏层数量,32 // 2 = 16，表示模型包含 16 层 Transformer
+        num_attention_heads=32 // 2, # 每个 Transformer 层中多头注意力机制的注意力头数量
+        max_position_embeddings=2048 // 2, # 最大位置嵌入数，即模型支持的最大序列长度
     )
 
     llamaModel = LlamaModel(config=llamaConfig).to(deivce)
@@ -25,6 +25,8 @@ def run_llama():
     input_ids = torch.randint(
         low=0, high=llamaConfig.vocab_size, size=(4, 30)).to(deivce)
     
+    # input_ids shape (batch_size, sequence_length)
+    # 4: batch_size, 30: sequence_length
     res = llamaModel(input_ids) # input_ids shape (batch_size, sequence_length)
     print(res)
 
